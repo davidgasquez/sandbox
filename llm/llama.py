4c05a76a92d1a3a947b9d82a86166bbe05c4326e
David Gasquez <davidgasquez@gmail.com>
2023-10-24 20:04:05 +0200

    feat: 🎨 more tweaking to llama models

--- llm/llama.py
+++ llm/llama.py
@@ -1,10 +1,9 @@
-import torch
 from llama_cpp import Llama
 
 llm = Llama(
-    model_path="mistral-7b-instruct-v0.1.Q4_K_M.gguf", n_gpu_layers=-1, main_gpu=1
+    model_path="mistral-7b-instruct-v0.1.Q4_K_M.gguf",
+    n_gpu_layers=-1, main_gpu=1
 )
-
 
 output = llm(
     "Q: Name the planets in the solar system and invent a new one. A: ",
@@ -14,4 +13,3 @@ output = llm(
 )
 
 print(output)
-print(torch.cuda.is_available())
