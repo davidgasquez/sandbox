ec068bfc7ba099cd154c14fafbcf7751022b145d
David Gasquez <davidgasquez@gmail.com>
2023-10-24 18:53:46 +0200

    feat: ✨ add llm playground

---
+++ llm/llama.py
@@ -0,0 +1,17 @@
+import torch
+from llama_cpp import Llama
+
+llm = Llama(
+    model_path="mistral-7b-instruct-v0.1.Q4_K_M.gguf", n_gpu_layers=-1, main_gpu=1
+)
+
+
+output = llm(
+    "Q: Name the planets in the solar system and invent a new one. A: ",
+    max_tokens=258,
+    stop=["Q:", "\n"],
+    echo=True,
+)
+
+print(output)
+print(torch.cuda.is_available())
