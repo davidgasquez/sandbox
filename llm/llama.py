384ccf06c711e01e8d52c80fa20cd607ceb55b27
David Gasquez <davidgasquez@gmail.com>
2023-10-25 16:12:00 +0200

    feat: ⚡️ llama working!

--- llm/llama.py
+++ llm/llama.py
@@ -1,13 +1,12 @@
 from llama_cpp import Llama
 
 llm = Llama(
-    model_path="mistral-7b-instruct-v0.1.Q4_K_M.gguf",
-    n_gpu_layers=-1, main_gpu=1
+    model_path="mistral-7b-instruct-v0.1.Q4_K_M.gguf", n_gpu_layers=-1, main_gpu=1
 )
 
 output = llm(
-    "Q: Name the planets in the solar system and invent a new one. A: ",
-    max_tokens=258,
+    "Q: Name the planets in the solar system and invent a 500 word story about them. A: ",
+    max_tokens=2048,
     stop=["Q:", "\n"],
     echo=True,
 )
