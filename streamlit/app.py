8b2123895f6ef85fa3d3507094b6d82f4cccbe68
David Gasquez <davidgasquez@gmail.com>
2024-05-21 17:16:17 +0200

    :memo:

--- streamlit/app.py
+++ streamlit/app.py
@@ -1,17 +1,3 @@
-import duckdb
-import pandas as pd
-
 import streamlit as st
 
-st.markdown("# Welcome to the DuckDB Streamlit Demo")
-
-c = duckdb.connect("md:fdp")
-
-st.write("Here's our first attempt at using data to create a table:")
-st.write(
-    pd.DataFrame({"first column": [1, 2, 3, 4], "second column": [10, 20, 30, 40]})
-)
-
-a = st.radio("Select one:", ["filecoin_storage_providers", "filecoin_clients"])
-
-st.write(c.sql(f"select * from main.{a}").df())
+st.markdown("# Hello, World!")
